f0299fe55038886abef85c76a3095d686d4f5eb5
Roman Granacher <roman.granacher@trinamix.de>
2021-10-14 12:53:14 +0200

    Updated

--- code/libone.cpp
+++ code/libone.cpp
@@ -3,9 +3,12 @@
 #include <iostream> 
 #include <algorithm>
 
-std::string tell_me_your_name()
+namespace v1
 {
-    return "My name is Slagathor";
+	std::string tell_me_your_name()
+	{
+		return "My name is Slagathor";
+	}
 }
 
 AnInterface::~AnInterface() = default;
@@ -13,19 +16,25 @@ AnInterface::~AnInterface() = default;
 class Concrete : public AnInterface
 {
 public:
-    ~Concrete() noexcept override = default;
-    virtual void do_something() override
-    {
-        std::cout << "doing something" << std::endl;
-    }
+	~Concrete() noexcept override = default;
+
+	virtual void do_something_new() override
+	{
+		std::cout << "doing something new" << std::endl;
+	}
+
+	virtual void do_something() override
+	{
+		std::cout << "doing something" << std::endl;
+	}
 };
 
 std::unique_ptr<AnInterface> give_me_an_interface()
 {
-    return std::make_unique<Concrete>();
+	return std::make_unique<Concrete>();
 }
 
-std::vector<int>::const_iterator find(const std::vector<int>& in, int x)
+std::vector<int>::const_iterator find(const std::vector<int>&in, int x)
 {
-    return std::find(in.begin(), in.end(), x);
+	return std::find(in.begin(), in.end(), x);
 }
--- code/libone.hpp
+++ code/libone.hpp
@@ -6,13 +6,19 @@
 
 #include "libone_export.h"
 
-LIBONE_EXPORT std::string tell_me_your_name();
+inline namespace v1 {}
+
+namespace v1
+{
+	LIBONE_EXPORT std::string tell_me_your_name();
+}
 
 class LIBONE_EXPORT AnInterface
 {
 public:
-    virtual ~AnInterface() = 0;
-    virtual void do_something() = 0;
+	virtual ~AnInterface() = 0;
+	virtual void do_something_new() = 0;
+	virtual void do_something() = 0;
 };
 
 LIBONE_EXPORT std::unique_ptr<AnInterface> give_me_an_interface();
